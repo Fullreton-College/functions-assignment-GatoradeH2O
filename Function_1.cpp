eea9c98db351ca9b7a030a5ebf3592933266869c
LuisT <Gutytrejo1502@gmail.com>
2026-03-08 15:03:07 -0700

    Implement coin flip function

--- Function_1.cpp
+++ Function_1.cpp
@@ -2,14 +2,24 @@
 #include <cstdlib>
 using namespace std;
 
-/* Define your function here */ 
+string coinflip()
+{
+   int flip = rand() %2; // returns 0 or 1
+   if (flip == 0)
+       return "Heads";
+   else
+       return "Tails";
+}
 
 int main() {
-   // Add more variables as needed
-   
+   int flips;
+   cin >> flips;
+
    srand(2);  // Unique seed
 
-   /* Type your code here */
+   for (int i = 0; i < flips; i++) {
+       cout << coinflip() << endl;
+   }
 
    return 0;
 }

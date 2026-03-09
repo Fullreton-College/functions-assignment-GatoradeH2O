a6e3155407331bd86cdb73916a2558ffbeb1e77e
LuisT <Gutytrejo1502@gmail.com>
2026-03-08 21:17:47 -0700

    Implement coin computation in Function_4.cpp

--- Function_4.cpp
+++ Function_4.cpp
@@ -2,12 +2,37 @@
 using namespace std;
 
 //define necessary functions
+void computeCoins(int coinValue, int& num, int& amountLeft);
 int main()
 {
+  int cents;
+  int quarters, dimes, pennies;
+  char choice = 'y';
 
-  //your code goes here
+while (choice == 'y' || choice == 'Y')
+{
+  cout << "Please enter change amount: ";
+  cin >> cents;
+
+  int amountLeft = cents;
+
+  computeCoins(25, quarters, amountLeft);
+  computeCoins(10, dimes, amountLeft);
+  computeCoins(1, pennies, amountLeft);
+
+  cout << "Quarters: " << quarters << endl;
+  cout << "Dimes: " << dimes << endl;
+  cout << "Pennies: " << pennies << endl;
+
+  cout << "Do you want to enter another amount? (Y/N): ";
+  cin >> choice;
+}
 
 return 0;
 }
 
-//implement functions
+void computeCoins(int coinValue, int& num, int& amountLeft)
+{
+  num = amountLeft / coinValue;
+  amountLeft = amountLeft % coinValue;
+}
